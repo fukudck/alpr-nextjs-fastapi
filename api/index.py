185245aba6b8ae2a8828fbf3f025b685d6c9ccf3
fukudck <75591795+fukudck@users.noreply.github.com>
2025-04-02 13:39:42 +0700

    update video api

--- api/index.py
+++ api/index.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI , File, UploadFile, HTTPException
+import gc
+from fastapi import FastAPI , File, UploadFile, HTTPException, BackgroundTasks
 from fastapi.responses import JSONResponse
 import os
 import shutil
-
+import time
+import uuid
+import redis
+import json
+from pydantic import BaseModel
 import time
 import cv2 as cv
 import numpy as np
@@ -13,7 +18,7 @@ import torch
 from api.wpodnet.backend import Predictor
 from api.wpodnet.model import WPODNet
 from PIL import Image
-
+import datetime
 import re
 import os
 
@@ -45,24 +50,24 @@ class RecognitionModel:
         return self.recognition_model(frame, verbose=False, classes = (2,3,5,7))[0]
 
 class WPODNET:
-  def __init__(self, alpr_model_weights):
-    self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    self.wpodnet_model = self.load_model(alpr_model_weights)
-    self.wpodnet_predictor = self.load_predictor(self.wpodnet_model)
+    def __init__(self, alpr_model_weights):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.wpodnet_model = self.load_model(alpr_model_weights)
+        self.wpodnet_predictor = self.load_predictor(self.wpodnet_model)
 
-  def load_model(self,alpr_model_weights):
-    model = WPODNet()
-    model.to(self.device)
+    def load_model(self, alpr_model_weights):
+        model = WPODNet()
+        model.to(self.device)
 
-    checkpoint = torch.load(alpr_model_weights)
-    model.load_state_dict(checkpoint)
+        checkpoint = torch.load(alpr_model_weights)
+        model.load_state_dict(checkpoint)
 
-    return model
+        return model
 
-  def load_predictor(self, model):
-    predictor = Predictor(model)
+    def load_predictor(self, model):
+        predictor = Predictor(model)
 
-    return predictor
+        return predictor
 
 use_correct_rules = True #@param {type:"boolean"}
 
@@ -187,24 +192,28 @@ class POCR:
         return -1, -1
 
 class ALPR:
-    def __init__(self, recognition_model_name, alpr_model_weights ):
-        self.recognition_model = RecognitionModel(recognition_model_name)
+    def __init__(self, recognition_model_name, alpr_model_weights):
+        self.recognition_model_name = recognition_model_name
+        self.image_recognition_model = RecognitionModel(self.recognition_model_name)
         self.wpodnet = WPODNET(alpr_model_weights)
         self.wpodnet_model = self.wpodnet.wpodnet_model
         self.wpodnet_predictor = self.wpodnet.wpodnet_predictor
         self.paddle_ocr = POCR()
-        self.paused =False
-    def run_img(self, img_path):
+    def run_img(self, img_path, run_uuid):
         img = cv.imread(img_path)
-        results = self.recognition_model.predict_without_track(img)
-
+        results = self.image_recognition_model.predict_without_track(img)
         all_texts = []  # Danh sách lưu kết quả OCR
-        
+        save_path = f'content/{run_uuid}'
+        order = 0
+
+        os.makedirs(save_path, exist_ok=True)  # Tạo thư mục riêng theo UUID
         for r in results:
             result = r.boxes.cpu()
             b = result.xyxy
+            order += 1
 
             for i, loc in enumerate(b):
+                
                 x1, x2 = int(loc[0]), int(loc[2])
                 y1, y2 = int(loc[1]), int(loc[3])
 
@@ -227,32 +236,44 @@ class ALPR:
                 # paddle ocr
                 annotated_lp = lpr_frame_prediction.annotate()
                 annotated_lp = np.asarray(annotated_lp)
-                # cv.imwrite(f'content/annotated_{i}.jpg', annotated_lp)
+                cv.imwrite(f'{save_path}/annotated_{order}.jpg', annotated_lp)
 
                 warped_lp = lpr_frame_prediction.warp()
                 warped_lp = np.asarray(warped_lp)
-                # cv.imwrite(f'content/warped_{i}.jpg', warped_lp)
+                cv.imwrite(f'{save_path}/warped_{order}.jpg', warped_lp)
                 # warped_lp = paddle_ocr.preprocess(warped_lp)
                 # warped_lp = paddle_ocr.preprocess(img_bounds)
 
                 text, score = self.paddle_ocr.ocr(warped_lp)
-                all_texts.append({"text": text, "score": score})
-        return all_texts 
-
+                all_texts.append({ "order": order, "text": text, "score": score})
+        return {"uuid": run_uuid, "type": "image", "status": "completed", "results": all_texts}
+    
     def plot_boxes(self, results ,frame, isSkip):
         annotator = Annotator(frame)
+        plates = []
         for r in results:
             result = r.boxes.cpu()
             if result.id is None:
                 continue
 
             objects_id = result.id
-            for i in range(len(result.xyxy)):
+            target_objects = ['car', 'motorcycle', 'bus', 'truck']
+            location_idx = []
+            for tar in target_objects:
+                temp_id = list(r.names.keys())[list(r.names.values()).index(tar)]
+                temp_check = np.where(result.cls == temp_id)
+                if len(temp_check[0]) > 0:
+                    for temp in temp_check[0]:
+                        location_idx.append((temp,tar))
+            
+            
+            
+            for i,vehicle in location_idx:
                 b = result.xyxy[i]
                 object_id = int(objects_id[i].item())
                 x1, y1, x2, y2 = map(int, b)
                 annotator.box_label((x1, y1, x2, y2), str(object_id), color=(0, 0, 255), txt_color=(255, 255, 255))
-
+                
                 #POCR
                 if not isSkip:
                     vehicle_roi = frame[y1:y2, x1:x2]
@@ -261,67 +282,59 @@ class ALPR:
                     if vehicle_roi.shape[0] > 0 and vehicle_roi.shape[1] > 0:
                         vehicle_image = Image.fromarray(cv.cvtColor(vehicle_roi, cv.COLOR_BGR2RGB))
                         lpr_frame_prediction = self.wpodnet_predictor.predict(vehicle_image, scaling_ratio=1)
-                        if lpr_frame_prediction.confidence > 0.8:
+                        if lpr_frame_prediction.confidence > 0.5:
                             warped_lp = lpr_frame_prediction.warp()
                             warped_lp = np.asarray(warped_lp)
                             text, score = self.paddle_ocr.ocr(warped_lp)
-                            text = f"{object_id}, {text}{score}"
-                            print(text)
+                            # text = f"{object_id}, {text}    {score}"
+                            # print(text)
+                            # print(vehicle)
+                            plates.append({"vehicle_id": object_id, "text": text, "confidence": score, "vehicle_type": vehicle, "plate_image": "None"})
 
-        return annotator.result()
-
-    def cv_show(self, frame, output):
-        output.write(frame)
-        scale_percent = 100  # Giảm kích thước xuống 50%
-        width = int(frame.shape[1] * scale_percent / 100)
-        height = int(frame.shape[0] * scale_percent / 100)
-        resized_frame = cv.resize(frame, (width, height))
-
-        cv.imshow("ALPR Detection", resized_frame)
-
-    def run_video(self, video_path, output_path, n_skip_frame):
+            
+        return annotator.result(), plates
+    
+    def run_video(self, video_path, n_skip_frame):
         start_time = time.time()
         cap = cv.VideoCapture(video_path)
         self.frame_width = int(cap.get(3))
         self.frame_height = int(cap.get(4))
         fps = int(cap.get(5))
         size = (self.frame_width, self.frame_height)
-        output = cv.VideoWriter(output_path+"output.mp4",
+        output = cv.VideoWriter("uploads/output.mp4",
                                 cv.VideoWriter_fourcc(*'mp4v'),
                                 fps, size)
-        n_frame = 0
-        last_results = None  # Lưu kết quả nhận diện của frame trước
-
+        results = []
+        last_results = None
+        recognition_model = RecognitionModel(self.recognition_model_name)
+        
 
         while cap.isOpened():
-            if not self.paused:
-                success, frame = cap.read()
-                n_frame += 1
-                if not success:
-                    break
-                if n_frame % n_skip_frame != 0:
-                    if last_results is not None:
-                        annotated_frame = self.plot_boxes(last_results, frame, True)
-                    else:
-                        annotated_frame = frame
-                    # print(f'Skip frame {n_frame}')
-                    self.cv_show(annotated_frame, output)
-                    continue
-                last_results = self.recognition_model.predict(frame)
-
-                annotated_frame = self.plot_boxes(last_results, frame, False)
-                self.cv_show(annotated_frame, output)
-
-            key = cv.waitKey(1) & 0xFF
-            if key == ord('q'):
+            success, frame = cap.read()
+            n_frame = int(cap.get(cv.CAP_PROP_POS_FRAMES))
+            if not success:
                 break
-            elif key == ord('p'):
-                self.paused = not self.paused
+            if n_frame % n_skip_frame != 0:
+                if last_results is not None:
+                    annotated_frame, plates = self.plot_boxes(last_results, frame, True)
+                else:
+                    annotated_frame = frame
+                output.write(annotated_frame)
+                continue
+            last_results = recognition_model.predict(frame)
+
+            annotated_frame, plates = self.plot_boxes(last_results, frame, False)
+            if plates:
+                results.append({"timestamp": frame_index_to_timestamp(n_frame, fps),"frame_number": n_frame, "plates": plates})
+            output.write(annotated_frame)
         cap.release()
         cv.destroyAllWindows()
         end_time = time.time()
         elapsed_time = end_time - start_time
-        print("elapsed_time:{0}".format(elapsed_time) + "[sec]")
+        del recognition_model
+        gc.collect()
+        
+        return {"uuid": "video", "type": "video", "status": "completed","process_time": elapsed_time, "results": results}
 
 def is_image(file_path: str) -> bool:
     # Check using imghdr
@@ -335,6 +348,7 @@ def is_image(file_path: str) -> bool:
         return True
     except Exception:
         return False
+
 def is_video(file_path: str) -> bool:
     cap = cv.VideoCapture(file_path)
     if cap.isOpened():
@@ -342,16 +356,53 @@ def is_video(file_path: str) -> bool:
         return True
     return False
 
+def frame_index_to_timestamp(frame_index: int, fps: float) -> str:
+    """
+    Chuyển số thứ tự frame thành timestamp định dạng HH:MM:SS.sss
+    
+    Args:
+        frame_index (int): Số thứ tự của frame (bắt đầu từ 0).
+        fps (float): Số khung hình trên giây.
+    
+    Returns:
+        str: Timestamp ở định dạng HH:MM:SS.sss
+    """
+    if fps <= 0:
+        raise ValueError("FPS phải lớn hơn 0")
+    if frame_index < 0:
+        raise ValueError("Số thứ tự frame phải >= 0")
+    
+    # Tính số giây
+    total_seconds = frame_index / fps
+    
+    # Chuyển đổi thành định dạng HH:MM:SS.sss
+    timestamp = str(datetime.timedelta(seconds=total_seconds))
+    
+    # Định dạng lại để luôn có 3 chữ số phần nghìn giây
+    if "." in timestamp:
+        hh_mm_ss, ms = timestamp.split(".")
+        timestamp = f"{hh_mm_ss}.{ms[:3].ljust(3, '0')}"
+    else:
+        timestamp += ".000"
+    
+    return timestamp
 
 model_path = "api/model/yolov8n.pt"  # Model nhận diện phương tiện
 img_path = 'docs/sample/original/3.jpg'
 weights_path = "api/weights/wpodnet.pth"
+UPLOAD_FOLDER = "uploads" 
 
 alpr = ALPR(model_path, weights_path)
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
-UPLOAD_FOLDER = "uploads" 
+
+redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+
+
+
+
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/api/py/helloFastApi")
@@ -360,10 +411,13 @@ def hello_fast_api():
 
 
 @app.post("/api/image_LPR")
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file1(file: UploadFile = File(...)):
     if not file:
         raise HTTPException(status_code=400, detail="No file part")
-    
+    task_uuid = str(uuid.uuid4())
+    # file_ext = os.path.splitext(file.filename)[-1].lower()
+    # file_name = f"{task_uuid}{file_ext}"
+    # file_path = os.path.join(UPLOAD_FOLDER, file_name)
     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -373,18 +427,26 @@ async def upload_file(file: UploadFile = File(...)):
         os.remove(file_path)  # Delete invalid file
         raise HTTPException(status_code=400, detail="Uploaded file is not a valid image")
     
-    text = alpr.run_img(file_path)
+    text = alpr.run_img(file_path, task_uuid)
     return JSONResponse(content=text)
 
+@app.post("/api/video_LPR")
+async def upload_file(file: UploadFile = File(...)):
+    if not file:
+        raise HTTPException(status_code=400, detail="No file part")
+    task_uuid = str(uuid.uuid4())
+    # file_ext = os.path.splitext(file.filename)[-1].lower()
+    # file_name = f"{task_uuid}{file_ext}"
+    # file_path = os.path.join(UPLOAD_FOLDER, file_name)
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    
+    # Validate if the uploaded file is an image
+    if not is_video(file_path):
+        os.remove(file_path)  # Delete invalid file
+        raise HTTPException(status_code=400, detail="Uploaded file is not a valid video")
+    
+    text = alpr.run_video(file_path, 5)
+    return JSONResponse(content=text)
 
-# @app.post("/api/video_LPR")
-# async def upload_file(file: UploadFile = File(...)):
-#     if not file:
-#         raise HTTPException(status_code=400, detail="No file part")
-#     file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-#     with open(file_path, "wb") as buffer:
-#         shutil.copyfileobj(file.file, buffer)
-
-#     if not is_video(file_path):
-#         os.remove(file_path)
-#         raise HTTPException(status_code=400, detail="Invalid file type")
